201c7aacb146616250d8d7c981b8cabb8b72b6e8
SantinoRusso <santino.russo04@gmail.com>
2023-01-17 16:25:43 -0300

    agregando cambios

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const app = express();
 const publicPath = path.resolve(__dirname,  './public');
 app.use( express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('servidor corriendo en el puerto 3000');
-});
+app.listen(5000, () => 
+    console.log('servidor corriendo en el puerto 5000'));
 
-app.get('/', (req, res) => {
+app.get('/home.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+app.get("/register.html", (req, res) => {
+ res,sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login.html", (req, res) => {
+    res,sendFile(path.resolve(__dirname, "./views/login.html"));
+   });
