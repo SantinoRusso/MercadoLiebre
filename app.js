baf6f4902a567269a847893e157d747e6ea2d2d5
SantinoRusso <santino.russo04@gmail.com>
2023-05-11 23:20:15 -0300

    entrega x2

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname,  './public');
-app.use( express.static(publicPath));
+const publicFolderPath = path.resolve(__dirname, './public');
+console.log(publicFolderPath);
 
-const port = process.env.PORT || 3001;
+app.use(express.static(publicFolderPath) );
 
-
-app.get('/home.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-app.get("/register.html", (req, res) => {
- res,sendFile(path.resolve(__dirname, "./views/register.html"));
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
 
-app.get("/login.html", (req, res) => {
-    res,sendFile(path.resolve(__dirname, "./views/login.html"));
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
 });
-app.listen(port, () =>console.log(`Servidor corriendo en el puerto ${port}`));
-//npm test
+
+
+const port = process.env.PORT || 3000;
+app.listen( port, ()=> console.log(`Servidor corriendo en el puerto ${port}`));
